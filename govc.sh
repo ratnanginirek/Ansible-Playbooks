dfebea06321a866fa1ae20bd3aeac8d332388e00
ratnanginirek <ratnanginirek@gmail.com>
2018-08-20 10:18:55 -0500

    Update govc.sh

--- govc.sh
+++ govc.sh
@@ -6,10 +6,10 @@
      govc version
 
      export GOVC_INSECURE=1
-     export GOVC_URL=10.234.96.69
-     export GOVC_USERNAME="lab\nirekr1"
-     export GOVC_PASSWORD="ChangeMe!"
-     export GOVC_DATASTORE="VNX5400-SW-Build-02"
+     export GOVC_URL = x.x.x.x
+     export GOVC_USERNAME="*****"
+     export GOVC_PASSWORD="*****"
+     export GOVC_DATASTORE="*****"
      export GOVC_NETWORK=SW_Build_198
      export GOVC_RESOURCE_POOL=*/Resources/*
      export GOVC_DATACENTRE=Infrastructure
